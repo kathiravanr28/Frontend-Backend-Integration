43da7b22bf47850ad0f16cd706cfd03bf7b5a007
Kathir <kathiravanr280303@gmail.com>
2026-02-02 23:11:36 +0530

    first commit
    I created the required files and dependencies for the Frontend-Backend-Integration Project.

---
+++ backend/src/main/java/frontend_Backend_Integration/demo/FrontendBackendApplication.java
@@ -0,0 +1,13 @@
+package frontend_Backend_Integration.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendBackendApplication.class, args);
+	}
+
+}
